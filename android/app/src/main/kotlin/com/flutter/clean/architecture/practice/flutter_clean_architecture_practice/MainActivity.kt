be0570ef283367852055f709806965331f3d4c64
Paksi Tamtomo Aji <paksiaji.1@gmail.com>
2023-08-01 14:47:30 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/flutter/clean/architecture/practice/flutter_clean_architecture_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.clean.architecture.practice.flutter_clean_architecture_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
